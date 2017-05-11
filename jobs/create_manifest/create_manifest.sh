03d31393fa1057dd38df2405f555e1ee1decbaab
Brian <brian.parry@dell.com>
2017-05-11 13:20:54 -0400

    WIP POC echo manifest

--- jobs/create_manifest/create_manifest.sh
+++ jobs/create_manifest/create_manifest.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 set -ex
 pushd $WORKSPACE
-./on-build-config/build-release-tools/HWIMO-BUILD on-build-config/build-release-tools/application/generate_manifest.py \
---branch "$branch" \
---date "$date" \
---timezone "$timezone" \
---builddir b \
---force \
---jobs 8
+if [[ -z $MANIFEST_NAME ]]; then
+    ./on-build-config/build-release-tools/HWIMO-BUILD on-build-config/build-release-tools/application/generate_manifest.py \
+    --branch "$branch" \
+    --date "$date" \
+    --timezone "$timezone" \
+    --builddir b \
+    --force \
+    --jobs 8
+    arrBranch=($(echo $branch | tr "/" "\n"))
+    slicedBranch=${arrBranch[-1]}
+    manifest_name=$(find -maxdepth 1 -name "$slicedBranch-[0-9]*" -printf "%f\n")
+else
+    manifest_name=$MANIFEST_NAME
+fi
 
-arrBranch=($(echo $branch | tr "/" "\n"))
-slicedBranch=${arrBranch[-1]}
-manifest_name=$(find -maxdepth 1 -name "$slicedBranch-[0-9]*" -printf "%f\n")
 
-./on-build-config/build-release-tools/pushToBintray.sh \
---user $BINTRAY_USERNAME \
---api_key $BINTRAY_API_KEY \
---subject $BINTRAY_SUBJECT \
---repo $BINTRAY_REPO \
---package manifest \
---version $manifest_name \
---file_path $branch-*
+if [[ -z $MANIFEST_NAME ]]; then
+    ./on-build-config/build-release-tools/pushToBintray.sh \
+    --user $BINTRAY_USERNAME \
+    --api_key $BINTRAY_API_KEY \
+    --subject $BINTRAY_SUBJECT \
+    --repo $BINTRAY_REPO \
+    --package manifest \
+    --version $manifest_name \
+    --file_path $branch-*
+fi
 
 MANIFEST_FILE_URL=https://dl.bintray.com/$BINTRAY_SUBJECT/$BINTRAY_REPO/$manifest_name
 echo "MANIFEST_FILE_URL=<a href=\"$MANIFEST_FILE_URL\">$manifest_name</a>"
